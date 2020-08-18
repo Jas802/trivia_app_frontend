562890496e416c2bd954b05a3eb689933d4f595f
Jas802 <jas.spencer414@gmail.com>
2020-08-18 15:56:56 -0400

    mapped questions to array

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ fetch('http://localhost:3000/api/v1/questions').then((response) => {
     console.log('resolved', response);
     return response.json();
 }).then(data => {
-    console.log(data);
+    console.log(data.data);
+    const questions = data.data.map(question => {
+        return `<p>Question ${question.title}</p>`
+    }).join("");
+    document.querySelector('#app').insertAdjacentHTML('afterbegin', questions); 
 }).catch((err) => {
     console.log('rejected', err);
 });
@@ -12,7 +16,7 @@ fetch('http://localhost:3000/api/v1/answers').then((response) => {
     console.log('resolved', response);
     return response.json();
 }).then(data => {
-    console.log(data);
+    console.log(data.data);
 }).catch((err) => {
     console.log('rejected', err);
 });
