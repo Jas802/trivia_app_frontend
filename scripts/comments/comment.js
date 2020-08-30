efdc20555c9c7c835e36eab616a082e722f11ca8
Jas802 <jas.spencer414@gmail.com>
2020-08-30 15:09:26 -0400

    comment styling functions

--- scripts/comments/comment.js
+++ scripts/comments/comment.js
@@ -4,6 +4,26 @@ class Comment {
         this.content = comment.content
     }
 }
+
+var field = document.querySelector('textarea');
+var backUp = field.getAttribute('placeholder');
+var btn = document.querySelector('.btn');
+var clear = document.getElementById('cancelButton');
+field.onfocus = function() {
+    this.setAttribute('placeholder', '');
+    this.style.borderColor = '#333'
+    btn.style.display = 'block'
+}
+
+field.onblur = function() {
+    this.setAttribute('placeholder', backUp)
+    this.style.borderColor = '#aaa';
+}
+
+clear.onclick = function() {
+    btn.style.display = 'none';
+    field.value = '';
+};
 const commentBaseUrl = 'http://localhost:3000/api/v1/comments';
 
 fetch(commentBaseUrl, {
