e7d238deeab76df160acd99b5fe542c3ceb2578b
Jas802 <jas.spencer414@gmail.com>
2020-08-30 19:54:18 -0400

    finished post comments

--- scripts/comments/comment.js
+++ scripts/comments/comment.js
@@ -4,11 +4,14 @@ class Comment {
         this.content = comment.content
     }
 }
-
+// Styling variables for comments
 var field = document.querySelector('textarea');
 var backUp = field.getAttribute('placeholder');
 var btn = document.querySelector('.btn');
 var clear = document.getElementById('cancelButton');
+var commentForm = document.getElementById('comment-form');
+
+//Styling functions for comments
 field.onfocus = function() {
     this.setAttribute('placeholder', '');
     this.style.borderColor = '#333'
@@ -24,7 +27,16 @@ clear.onclick = function() {
     btn.style.display = 'none';
     field.value = '';
 };
-const commentBaseUrl = 'http://localhost:3000/api/v1/comments';
+
+commentForm.addEventListener('submit', postComment)
+
+function postComment(e) {
+    e.preventDefault();//page doesn't refresh
+    let commentValue = e.target.querySelector('textarea').value; //gets comment field
+    let commentData = {content:commentValue} //mapping for database
+
+    //Fetching and POST methods for comments
+    const commentBaseUrl = 'http://localhost:3000/api/v1/comments';
 
 fetch(commentBaseUrl, {
     method: "POST",
@@ -32,21 +44,21 @@ fetch(commentBaseUrl, {
         'Accept': 'application/json',
         "Content-Type": 'application/json',
     },
-    body: JSON.stringify(comment)
+    body: JSON.stringify(commentData)
 })
 .then(resp => resp.json())
 .then(comment => {
-      let com = new Comment(comment.id, comment.content)
-
-      com.renderComment();
+      let comObj = new Comment(comment.data)
+      renderComment(comObj);
+      field.value = '';
   })
 
-// renderComment() {
-//     let commentsDiv = document.getElementById("comments-container");
-//     commentsDiv.innerHTML +=
-//         `<div id="comments-container">
-//             <ul>
-//                 <li> ${this.content} </li>
-//             </ul>
-//         </div>`
-// };
+ function renderComment(c) {
+    let commentsUl = document.querySelector('#commentsList > ul');
+    let commentLi = document.createElement('li');
+    commentLi.textContent = c.content;
+    commentsUl.appendChild(commentLi);
+};
+    
+};
+
